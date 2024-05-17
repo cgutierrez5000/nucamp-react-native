d619097f775fad738cee4265c2780e4e6cc35af3
Carlos Gutierrez <c_e_gutierrez@yahoo.com>
2024-05-17 02:09:44 -0500

    Added new component CampsiteInfo

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -2,26 +2,24 @@ import { FlatList } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 
 const DirectoryScreen = (props) => {
-    const renderDirectoryItem = ({ item: campsite }) => {
-        return (
-            <ListItem>
-                <Avatar source={campsite.image} rounded />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>
-                        {campsite.description}
-                    </ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
-        );
-    };
+  const renderDirectoryItem = ({ item: campsite }) => {
     return (
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={(item) => item.id.toString()}
-        />
+      <ListItem onPress={() => props.onPress(campsite.id)}>
+        <Avatar source={campsite.image} rounded />
+        <ListItem.Content>
+          <ListItem.Title>{campsite.name}</ListItem.Title>
+          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
     );
+  };
+  return (
+    <FlatList
+      data={props.campsites}
+      renderItem={renderDirectoryItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
 };
 
 export default DirectoryScreen;
