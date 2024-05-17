12a1462ea85746e530e9df42e0bb443c3c36cc6a
Carlos Gutierrez <c_e_gutierrez@yahoo.com>
2024-05-17 00:40:24 -0500

    Cleaned up some files

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,25 +1,27 @@
-import { Flatlist } from 'react-native';
+import { FlatList } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 
 const DirectoryScreen = (props) => {
-  const renderDirectoryItem = ({ item: campsite }) => {
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem>
+                <Avatar source={campsite.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>
+                        {campsite.description}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
     return (
-      <ListItem>
-        <Avatar source={campsite.image} rounded />
-        <ListItem.Content>
-          <ListItem.Title>{campsite.name}</ListItem.Title>
-          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
+        <FlatList
+            data={props.campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
     );
-  };
-  return (
-    <Flatlist
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={(item) => item.id.toString()}
-    />
-  );
 };
 
 export default DirectoryScreen;
